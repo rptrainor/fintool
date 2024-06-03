82f1cf34f8994fc83f050d514f24303b4f6dc739
Ryan Trainor <rptrainor@gmail.com>
2024-06-03 02:28:04 -0300

    add tool tips

--- src/components/QueryForm.tsx
+++ src/components/QueryForm.tsx
@@ -10,8 +10,13 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormMessage,
 } from "~/components/ui/form"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip"
 import { Textarea } from "~/components/ui/textarea"
 import { toast } from "~/components/ui/use-toast"
 
@@ -37,73 +42,68 @@ export function QueryForm() {
   // flex min-h-[60px] w-full rounded-md border border-input px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 resize-none border-none outline-none ring-0 bg-transparent ring-none focus:ring-none focus:ring-offset-none
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="">
         <FormField
           control={form.control}
           name="query"
           render={({ field }) => (
-            <FormItem className="bg-white rounded-md">
+            <FormItem className="bg-white rounded-t-md rounded-b-none">
               <FormControl>
                 <Textarea
+                  autoFocus
                   placeholder="Ask about a company's filings&#8230;"
                   className="resize-none focus-visible:outline-none focus-visible:ring-0"
                   {...field}
                 />
               </FormControl>
-
-              <div className="flex flex-wrap gap-x-2 gap-y-1 items-center bg-gray-50 px-4 py-2.5 text-xs text-gray-700 rounded-md">
-                <button type="button" className="text-muted-foreground flex no-wrap gap-x-1 items-center">
-                  <kbd className="pointer-events-none px-1 py-1 h-7 w-7 flex justify-center items-center select-none gap-1 rounded border bg-muted font-mono font-medium text-muted-foreground opacity-100 text-base">
-                    &#36;
-                  </kbd>
-                </button>
-                <button type="button" className="text-muted-foreground flex no-wrap gap-x-1 items-center">
-                  <kbd className="pointer-events-none px-1 py-1 h-7 w-7 flex justify-center items-center select-none gap-1 rounded border bg-muted font-mono font-medium text-muted-foreground opacity-100 text-base">
-                    &#64;
-                  </kbd>
-                </button>
-                <button type="button" className="text-muted-foreground flex no-wrap gap-x-1 items-center">
-                  <kbd className="pointer-events-none px-1 py-1 h-7 w-7 flex justify-center items-center select-none gap-1 rounded border bg-muted font-mono font-medium text-muted-foreground opacity-100 text-base">
-                    &#47;
-                  </kbd>
-                </button>
-                <div className="grow" />
-                <Button className="rounded-full px-1 py-1 h-7 w-7 flex justify-center items-center" type="submit"><PaperAirplaneIcon className="h-4 w-4" /></Button>
-
-                {/* Type{' '}
-                  <kbd
-                    className={cn(
-                      'mx-1 flex h-5 w-5 items-center justify-center rounded border bg-white font-semibold sm:mx-2 border-gray-400 text-gray-900',
-                    )}
-                  >
-                    &#36;
-                  </kbd>{' '}
-                  <span className="sm:hidden">for companies,</span>
-                  <span className="hidden sm:inline">to access companies,</span>
-                  <kbd
-                    className={cn(
-                      'mx-1 flex h-5 w-5 items-center justify-center rounded border bg-white font-semibold sm:mx-2',
-                      rawQuery.startsWith('>') ? 'border-indigo-600 text-indigo-600' : 'border-gray-400 text-gray-900'
-                    )}
-                  >
-                    &gt;
-                  </kbd>{' '}
-                  for users, and{' '}
-                  <kbd
-                    className={cn(
-                      'mx-1 flex h-5 w-5 items-center justify-center rounded border bg-white font-semibold sm:mx-2',
-                      rawQuery.startsWith('>')? 'border-indigo-600 text-indigo-600' : 'border-gray-400 text-gray-900'
-                    )}
-                  >
-                    ?
-                  </kbd>{' '}
-                  for help. */}
-              </div>
-              {/* You can <span>@mention</span> other users and organizations. */}
-              <FormMessage />
             </FormItem>
           )}
         />
+        <TooltipProvider>
+          <div className="flex flex-wrap gap-x-2 gap-y-1 items-center bg-gray-50 px-4 py-2.5 text-xs text-gray-700 rounded-t-none rounded-b-md">
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="text-muted-foreground flex no-wrap gap-x-1 items-center">
+                  <kbd className="pointer-events-none px-1 py-1 h-7 w-7 flex justify-center items-center select-none gap-1 rounded border bg-muted font-mono font-medium text-muted-foreground opacity-100 text-base">
+                    &#36;
+                  </kbd>
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <span>companies</span>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="text-muted-foreground flex no-wrap gap-x-1 items-center">
+                  <kbd className="pointer-events-none px-1 py-1 h-7 w-7 flex justify-center items-center select-none gap-1 rounded border bg-muted font-mono font-medium text-muted-foreground opacity-100 text-base">
+                    &#64;
+                  </kbd>
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>documents</p>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="text-muted-foreground flex no-wrap gap-x-1 items-center">
+                  <kbd className="pointer-events-none px-1 py-1 h-7 w-7 flex justify-center items-center select-none gap-1 rounded border bg-muted font-mono font-medium text-muted-foreground opacity-100 text-base">
+                    &#47;
+                  </kbd>
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>suggestions</p>
+              </TooltipContent>
+            </Tooltip>
+
+            <div className="grow" />
+            <Button className="rounded-full px-1 py-1 h-7 w-7 flex justify-center items-center" type="submit">
+              <PaperAirplaneIcon className="h-4 w-4" />
+            </Button>
+          </div>
+        </TooltipProvider>
       </form>
     </Form>
   )
